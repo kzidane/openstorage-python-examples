61bdf7bda61a6fe9d1fe0068537d27d31886d2d6
wallnerryan <ryan.wallner@portworx.com>
2018-09-14 17:58:47 -0400

    add some k8s examples

---
+++ examples/kubernetes/create_vol.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+
+import os
+import grpc
+from openstorage import api_pb2
+from openstorage import api_pb2_grpc
+
+portworx_grpc = os.getenv('PWX_GRPC_ENDPOINT', 'localhost:9100')
+print "Connecting to %s" % portworx_grpc
+channel = grpc.insecure_channel(portworx_grpc)
+
+try:
+    # Cluster connection
+    clusters = api_pb2_grpc.OpenStorageClusterStub(channel)
+    ic_resp = clusters.InspectCurrent(api_pb2.SdkClusterInspectCurrentRequest())
+    print('Connected to {0} with status {1}'.format(
+        ic_resp.cluster.id,
+        api_pb2.Status.Name(ic_resp.cluster.status)
+    ))
+
+    # Create a volume
+    volumes = api_pb2_grpc.OpenStorageVolumeStub(channel)
+    v_resp = volumes.Create(api_pb2.SdkVolumeCreateRequest(
+        name="myvol",
+        spec=api_pb2.VolumeSpec(
+            size=100*1024*1024*1024,
+            ha_level=3,
+        )
+    ))
+    print('Volume id is {0}'.format(v_resp.volume_id))
+
+    # Create a snapshot
+    snap = volumes.SnapshotCreate(api_pb2.SdkVolumeSnapshotCreateRequest(
+        volume_id=v_resp.volume_id,
+        name="mysnap"
+    ))
+    print('Snapshot created with id {0}'.format(snap.snapshot_id))
+
+except grpc.RpcError as e:
+    print('Failed: code={0} msg={1}'.format(e.code(), e.details()))
---
+++ examples/kubernetes/list_volumes.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+
+import os
+import grpc
+from openstorage import api_pb2
+from openstorage import api_pb2_grpc
+
+portworx_grpc = os.getenv('PWX_GRPC_ENDPOINT', 'localhost:9100')
+print "Connecting to %s" % portworx_grpc
+channel = grpc.insecure_channel(portworx_grpc)
+
+try:
+    # Cluster connection
+    clusters = api_pb2_grpc.OpenStorageClusterStub(channel)
+    ic_resp = clusters.InspectCurrent(api_pb2.SdkClusterInspectCurrentRequest())
+    print('Connected to {0} with status {1}'.format(
+        ic_resp.cluster.id,
+        api_pb2.Status.Name(ic_resp.cluster.status)
+    ))
+
+    # List Volumes
+    volumes = api_pb2_grpc.OpenStorageVolumeStub(channel)
+    vols = volumes.Enumerate(api_pb2.SdkVolumeEnumerateRequest())
+    for vol in vols.volume_ids:
+        print('Volume id vol: {0}'.format(vol))
+
+except grpc.RpcError as e:
+    print('Failed: code={0} msg={1}'.format(e.code(), e.details()))
